2edc5acf6512ee2320517009816279b13a5a63b3
Andy LaBorde <andikthegreat@gmail.com>
2017-11-05 13:52:37 -0600

    Hospital assignment

--- Andy_LaBorde/Hospital/hospital.py
+++ Andy_LaBorde/Hospital/hospital.py
@@ -1,27 +1,53 @@
 class Patient(object):
-	unique_id = 0
+	count = 0
 	def __init__(self, name, allergies):
-		patient.unique_id +=1
-		self.idn = patient.unique_id
 		self.name = name
 		self.allergies = allergies
-		self.bednum = none
-	def info(self):
-		print "Name: {}, ID: {}, Allergies: {} Bed Number: {}".format(self.name, self.idn, self.allergies, self.bednum)
+		self.id = Patient.count
+		self.bed_num = None
+		Patient.count += 1
 
-pat1 = Patient('Jimmy', 'Peanuts')	
-pat2 = Patient(2, 'Slim', 'None', 'none')
+class Hospital(object):
+    def __init__(self, name, cap):
+	self.name = name
+	self.cap = cap
+	self.patients = []
+	self.beds = self.initialize_beds()
 
+    def initialize_beds(self):
+        beds = []
+        for i in range(0, self.cap):
+            beds.append({
+                "bed_id": i,
+                "Available": True
+            })
+        return beds
 
+    def admit(self, patient):
+        if len(self.patients) <= self.cap:
+            self.patients.append(patient)
+            for i in range(0, len(self.beds)):
+                if self.beds[i]["Available"]:
+                    patient.bed_num = self.beds[i]["bed_id"]
+                    self.beds[i]["Available"] = False
+                    break
+            print "Patient #{} admitted to bed #{}".format(patient.id, patient.bed_num)
+        else:
+            "Hospital is at full capacity"
+    def discharge(self, patient_id):
+        for patient in self.patients:
+            if patient.id == patient_id:
+                # free up bed
+                for bed in self.beds:
+                    if bed["bed_id"] == patient.bed_num:
+                        bed["Available"] = True
+                        break
 
+                self.patients.remove(patient)
+                return "Patient #{} sucessfully discharged.  Bed #{} now available".format(patient.id, patient.bed_num)
+        return "Patient not found"
 
-class Hospital(Patient):
-	def __init__(self, patients, Hname, cap):
-		self.patients = []
-		self.Hname = Hname
-		self.cap = cap
-		self.beds = 
+pat1=Patient('John Doe', 'Penicillin')
+h= Hospital('Little Hospital', 2)
 
-	
-
-
+h.admit(pat1)
