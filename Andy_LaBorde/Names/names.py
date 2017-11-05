d61525717e819bac0940c8b01306e1f5f9b49183
Andy LaBorde <andikthegreat@gmail.com>
2017-11-05 14:31:46 -0600

    Names Assignment

---
+++ Andy_LaBorde/Names/names.py
@@ -0,0 +1,33 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+for i in students:
+	print i ['first_name'] +' '+ i ['last_name']
+
+
+print "_____________"
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for i in users:
+	count = 0
+ 	for x in users[i]:
+ 		count += 1
+ 		print str(count) + ' '+ x['first_name']+ ' ' +x['last_name'] + ' ' +str(len(x['first_name']+x['last_name']))
+	 	
+	 		
